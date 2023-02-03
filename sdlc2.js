2ec6a81b43cb3ac6fab4e0711d4f5be4ca2d2b73
shehrbanoNiazi <120422470+shehrbanoNiazi@users.noreply.github.com>
2023-02-03 07:05:35 -0500

    Add files via upload

---
+++ sdlc2.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "stage includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes"
+const design = " outline the details for the overall application, alongside specific aspects" 
+const implementation = "developers actually write code and build the application according to the earlier design documents and outlined specifications"
+const testingAndIntegration ="it must be tested to make sure that there aren’t any bugs"
+ const maintain = " must now move into a maintenance mode and begin practicing any activities required to handle issues reported by end-users"
